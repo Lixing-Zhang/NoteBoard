443aa996f3e365ca86b3dd48a915cdf44aa6d98b
Jack Zhang <zlxjackie@hotmail.com>
2015-09-22 21:55:54 +1000

    Add edit and save state so note will be rendered between form and display

--- js/Note.js
+++ js/Note.js
@@ -3,15 +3,48 @@
  */
 
 var Note = React.createClass({
-  render : function() {
+  getInitialState: function() {
+    return { editing : false}
+  },
+
+  edit: function() {
+    this.setState({editing:true});
+  },
+
+  save:function() {
+    this.setState({editing:false});
+  },
+  remove: function() {
+    alert('removing note');
+  },
+
+  renderDisplay: function() {
     return (
         <div className="note">
+          <p>{this.props.children}</p>
           <span>
-            <button className="btn btn-primary glyphicon glyphicon-pencil"></button>
-            <button className="btn btn-danger glyphicon glyphicon-trash"></button>
+            <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"></button>
+            <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"></button>
           </span>
         </div>
     );
+  },
+
+  renderForm: function() {
+    return (
+        <div className="note">
+            <textarea defaultValue={this.props.children} className="form-control"></textarea>
+          <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk"></button>
+        </div>
+    );
+  },
+
+  render : function() {
+    if(this.state.editing) {
+      return this.renderForm();
+    } else {
+      return this.renderDisplay();
+    }
   }
 });
 
