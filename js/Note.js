626962d34d72aa12da9fe2d32ab025e4ea088615
Jack Zhang <zlxjackie@hotmail.com>
2015-10-01 21:49:07 +1000

    Add note-add button

--- js/Note.js
+++ js/Note.js
@@ -65,15 +65,15 @@ var Board = React.createClass({
 
   getInitialState: function () {
     return {
-      notes: [
-        'Call Bill',
-        'Call Bills',
-        'Call Bills',
-        'Call Billsss'
-      ]
+      notes: []
     };
   },
 
+  add: function(text) {
+    var arr = this.state.notes;
+    arr.push(text);
+    this.setState({notes: arr});
+  },
   update: function (newText, i) {
     var arr = this.state.notes;
     arr[i] = newText;
@@ -97,6 +97,7 @@ var Board = React.createClass({
     return (
         <div className="board">
           {this.state.notes.map(this.eachNote)}
+          <button className="btn btn-sm btn-success glyphicon glyphicon-plus" onClick={this.add.bind(null, "New Note")}></button>
         </div>
     );
   }
